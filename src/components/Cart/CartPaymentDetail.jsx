6c7dcb12011f6c88fbe335fb7b8431bec9866d03
Facundo Murga <fmurga2011@gmail.com>
2022-05-29 10:43:53 -0300

    Checkout route completed

--- src/components/Cart/CartPaymentDetail.jsx
+++ src/components/Cart/CartPaymentDetail.jsx
@@ -1,35 +1,20 @@
 import { CheckOutlined } from "@mui/icons-material";
-import React, { useContext } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
-import { CartContext } from "../../contexts/CartContextProvider";
+import PaymentDetail from "../extra/PaymentDetail";
 
 const CartPaymentDetail = () => {
-  const { subtotal, total, impuestos } = useContext(CartContext);
 
   return (
     <div className="container w-5/12 h-full bg-slate-200 mt-0 pt-6 p-10 m-10 rounded-md">
-      <div className="flex flex-col gap-2">
-        <h2 className="text-black border border-x-0 border-t-0 border-b-slate-50 pb-5 font-bold text-xl">
-          Detalle de Compra
-        </h2>
-        <p className="text-gray-500 border border-x-0 border-t-0 border-b-slate-50 py-2">
-          Subtotal: {subtotal || 0}
-        </p>
-        <p className="text-gray-500 border border-x-0 border-t-0 border-b-slate-50 py-2">
-          Impuestos: {impuestos || 0}
-        </p>
-        <div className="inline-flex justify-between">
-          <p className="text-black pt-2 font-bold text-lg">
-            Total: {total || 0}
-          </p>
-          <NavLink
-            to="/checkout"
-            className="px-3 py-2 w-max  text-white flex justify-center items-center cursor-pointer bg-purple-600 rounded-full hover:bg-purple-400 disabled:bg-purple-400 ">
-            <CheckOutlined />
-            Checkout
-          </NavLink>
-        </div>
-      </div>
+      <PaymentDetail>
+        <NavLink
+          to="/checkout"
+          className="px-3 py-2 w-max  text-white flex justify-center items-center cursor-pointer bg-purple-600 rounded-full hover:bg-purple-400 disabled:bg-purple-400 ">
+          <CheckOutlined />
+          Checkout
+        </NavLink>
+      </PaymentDetail>
     </div>
   );
 };
--- src/components/Checkout/CheckoutForm.jsx
+++ src/components/Checkout/CheckoutForm.jsx
@@ -1,18 +1,26 @@
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../contexts/CartContextProvider";
 import { db } from "../../firebase/firebaseConfig";
+import PaymentDetail from "../extra/PaymentDetail";
+import ModalAcceptBuy from "../Modals/ModalAcceptBuy";
 
 const CheckoutForm = () => {
   const { cartItems, total, clear } = useContext(CartContext);
-  const navigate = useNavigate();
+  const [submited, setSubmited] = useState(false);
+  const [orederId, setOrderId] = useState("");
+  const [open, setOpen] = useState(false);
   const initialForm = {
     name: "",
     email: "",
-    phone: 0,
+    phone: "",
   };
   const [form, setForm] = useState(initialForm);
+  const modalMessage = `Se ha creado tu compra con id ${orederId}`;
+  const modalTitle = "Compra Exitosa";
+
+  const navigate = useNavigate();
 
   const handleInputChange = (e) => {
     e.preventDefault();
@@ -35,76 +43,93 @@ const CheckoutForm = () => {
       total: total,
     };
     const ordersColl = collection(db, "orders");
-    addDoc(ordersColl, order).then(() => {
+    addDoc(ordersColl, order).then(({ id }) => {
+      setOrderId(id);
+      setOpen(true);
+    });
+  };
+  
+
+  useEffect(() => {
+    if (submited) {
       setForm(initialForm);
       clear();
       navigate("/");
-    });
-  };
+    }
+  }, [submited]);
 
   return (
-    <div className="w-5/12 h-full rounded-md">
-      <div className="leading-loose">
-        <form
-          className="w-full p-10 bg-white rounded shadow-xl"
-          onSubmit={handleSubmit}>
-          <h2 className="text-gray-800 font-medium text-xl py-2">
-            Pagar Compra
-          </h2>
-          <div className="mt-2">
-            <label className="block text-sm text-gray-700" htmlFor="name">
-              Nombre
-            </label>
-            <input
-              onChange={(e) => handleInputChange(e)}
-              className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
-              id="name"
-              name="name"
-              type="text"
-              required
-              aria-label="Name"
-              value={form.name}
-            />
-          </div>
-          <div className="mt-2 w-96">
-            <label className="block text-sm text-gray-600" htmlFor="email">
-              Email
-            </label>
-            <input
-              onChange={(e) => handleInputChange(e)}
-              className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
-              name="email"
-              type="email"
-              required
-              aria-label="Email"
-              value={form.email}
-            />
-          </div>
-          <div className=" mt-2 w-96">
-            <label className=" block text-sm text-gray-600" htmlFor="phone">
-              Telefono
-            </label>
-            <input
-              onChange={(e) => handleInputChange(e)}
-              className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
-              name="phone"
-              type="tel"
-              required
-              aria-label="Phone"
-              value={form.phone}
-            />
-          </div>
-          <div className="mt-4 w-96">
-            <button
-              className="px-4 py-1 text-white font-bold bg-gray-900 rounded"
-              type="submit"
-              onClick={handleSubmit}>
-              Comprar
-            </button>
-          </div>
-        </form>
+    <>
+      <div className="w-5/12 h-full rounded-md">
+        <div className="leading-loose">
+          <form
+            className="w-full p-10 bg-white rounded shadow-xl"
+            onSubmit={handleSubmit}>
+            <h2 className="text-gray-800 font-medium text-xl py-2">
+              Pagar Compra
+            </h2>
+            <div className="mt-2">
+              <label className="block text-sm text-gray-700" htmlFor="name">
+                Nombre
+              </label>
+              <input
+                onChange={(e) => handleInputChange(e)}
+                className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
+                id="name"
+                name="name"
+                type="text"
+                aria-label="Name"
+                required={true}
+                value={form.name}
+              />
+            </div>
+            <div className="mt-2">
+              <label className="block text-sm text-gray-600" htmlFor="email">
+                Email
+              </label>
+              <input
+                onChange={(e) => handleInputChange(e)}
+                className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
+                name="email"
+                type="email"
+                aria-label="Email"
+                required={true}
+                value={form.email}
+              />
+            </div>
+            <div className=" mt-2">
+              <label className=" block text-sm text-gray-600" htmlFor="phone">
+                Telefono
+              </label>
+              <input
+                onChange={(e) => handleInputChange(e)}
+                className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
+                name="phone"
+                type="tel"
+                aria-label="Phone"
+                required={true}
+                value={form.phone}
+              />
+            </div>
+            <PaymentDetail />
+            <div className="mt-4">
+              <button
+                className="px-4 py-1 text-white font-bold bg-gray-900 rounded"
+                type="submit">
+                Comprar
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+      <ModalAcceptBuy
+        open={open}
+        setOpen={setOpen}
+        title={modalTitle}
+        message={modalMessage}
+        submited={setSubmited}
+      />
+    </>
   );
 };
 
